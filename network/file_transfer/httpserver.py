d346ccb191c29bdeac7ed18f322521ea37be6b07
Mo_s <mo_s@foxmail.com>
2018-02-23 12:28:32 +0800

    add file_transfser python bind local ip

---
+++ network/file_transfer/httpserver.py
@@ -0,0 +1,37 @@
+import sys
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import BaseHTTPServer
+
+# bind interface
+# if python3
+# python3 -m http.server 15000 --bind 127.0.0.1
+
+def http_server_start(HandlerClass=SimpleHTTPRequestHandler,
+         ServerClass=BaseHTTPServer.HTTPServer):
+
+    protocol = "HTTP/1.0"
+    host = ''
+    port = 8000
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+        if ':' in arg:
+            host, port = arg.split(':')
+            port = int(port)
+        else:
+            try:
+                port = int(sys.argv[1])
+            except:
+                host = sys.argv[1]
+
+    server_address = (host, port)
+
+    HandlerClass.protocol_version = protocol
+    httpd = ServerClass(server_address, HandlerClass)
+
+    sa = httpd.socket.getsockname()
+    print "Serving HTTP on", sa[0], "port", sa[1], "..."
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    http_server_start()
